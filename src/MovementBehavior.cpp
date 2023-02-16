031573a06216dfd047c5c0f96b810693009e5ba8
Mateus paula rodrigues <mateus_p_r@hotmail>
2023-02-16 17:24:48 -0300

    move

---
+++ src/MovementBehavior.cpp
@@ -0,0 +1,43 @@
+#include "MovementBehavior.h"
+
+MovementBehavior::MovementBehavior(GameObject& associated, float speed , float duration , int type ):GameObject(associated){
+    this->speed = speed;
+    this->duration = duration;
+    this->type = type;
+    fase = 0;
+    timer.SetFinish(duration);
+
+}
+MovementBehavior::~MovementBehavior(){
+    
+}
+void MovementBehavior::Update(float dt){
+    if(timer.Update(dt)){
+        fase = !fase;
+        timer.Restart();
+    }
+
+    switch(type){
+        case 0:
+
+            if(fase){
+
+                associated.box.x -= speed*dt;
+               
+            }else{
+                associated.box.x += speed*dt;
+            }
+                
+           
+    }
+}
+
+void MovementBehavior::Render(){
+
+}
+bool MovementBehavior::Is(std::string type){
+
+}
+bool MovementBehavior::Is(C_ID){
+
+}
